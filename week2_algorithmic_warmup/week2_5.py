45d3e844832c14adfce74d84bbfdcebd2056b441
Iamworlord <57576075+Iamworlord@users.noreply.github.com>
2021-01-08 01:02:27 +0530

    Update week2_5.py

--- week2_algorithmic_warmup/week2_5.py
+++ week2_algorithmic_warmup/week2_5.py
@@ -3,18 +3,15 @@ def pisanoPeriod(m):
     for i in range(0, m * m):
         previous, current \
             = current, (previous + current) % m
-
-        # A Pisano Period starts with 01
         if (previous == 0 and current == 1):
             return i + 1
 
 
 def fibonacciModulo(n, m):
-    # Getting the period
+    
     pisano_period = pisanoPeriod(m)
 
-    # Taking mod of N with
-    # period length
+  
     n = n % pisano_period
 
     previous, current = 0, 1
